787aaf3afbe54f0a923c38f829389993445c79fc
Jason R. Coombs <jaraco@jaraco.com>
2023-05-29 04:02:32 -0400

    Experimental work on a prefix class, inspired by pypi:env.

--- jaraco/env.py
+++ jaraco/env.py
@@ -1,4 +1,37 @@
+import collections
 import os
+
+
+class Prefix(collections.abc.Mapping):
+    """
+    >>> setenv = getfixture('monkeypatch').setenv
+    >>> setenv('JARACO_ENV_FOO', 'foo')
+    >>> setenv('JARACO_ENV_BAR', 'bar')
+    >>> env = Prefix('jaraco_env_')
+    >>> dict(env)
+    {'foo': 'foo', 'bar': 'bar'}
+    >>> bool(env.check('foo', expect='foo'))
+    True
+    """
+
+    def __init__(self, value):
+        self.value = value.lower()
+
+    def __iter__(self):
+        return (
+            val.lower().removeprefix(self.value)
+            for val in os.environ
+            if val.lower().startswith(self.value)
+        )
+
+    def __len__(self):
+        return len(tuple(iter(self)))
+
+    def __getitem__(self, key):
+        return os.environ[self.value.upper() + key.upper()]
+
+    def check(self, *args, **kwargs):
+        return Check(*args, lookup=self, **kwargs)
 
 
 class Check:
